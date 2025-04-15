b78ddc2bb33584199e24ed75a2ec046fca436d0a
tamakanshika <anshikatamak27@gmail.com>
2025-04-15 12:11:27 +0530

    Initial Commit

---
+++ src/main/java/com/example/ci_cd_test/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.ci_cd_test;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String homepage() {
+        return "hi! everyone this is anshika";
+    }
+
+
+}
---
+++ src/test/java/com/example/ci_cd_test/CiCdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ci_cd_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
